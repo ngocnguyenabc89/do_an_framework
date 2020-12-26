f7816156781deccc7e930b372ee80d18af1322d3
uyenguyen8974 <18520396@gm.uit.edu.vn>
2020-12-26 21:23:19 +0700

    Cap nhat product

---
+++ do_an_framework/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace do_an_framework.Models
+{
+    public class ProductModel
+    {
+        public int product_id { get; set; }
+        public string product_name { get; set; }
+        public string product_discription { get; set; }
+        public int prodoct_price { get; set; }
+        public int product_state { get; set; }
+        public int product_kind { get; set; }
+        public string product_image { get; set; }
+        public string product_list { get; set; }
+        public DateTime product_created_at { get; set; }
+
+        public string category_name { get; set; }
+    }
+}
